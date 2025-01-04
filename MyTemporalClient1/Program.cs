4b8cdcabfac2a42ab785e4187a55be16695dc4e3
Sujeeth KK <101649038+sujeethskk110@users.noreply.github.com>
2025-01-04 23:21:55 +0530

    Added Schedulers

--- MyTemporalClient1/Program.cs
+++ MyTemporalClient1/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using MyWorkFlows.Workflows;
 using Temporalio.Client;
 
@@ -6,26 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddSimpleConsole().SetMinimumLevel(LogLevel.Information);
 
-builder.Services.AddSingleton(ctx =>
-    TemporalClient.ConnectAsync(new()
-    {
-        TargetHost = "localhost:7233",
-        Namespace = "default"
-    }).Result); // Use .Result to get the TemporalClient instance synchronously
+var client = await TemporalClient.ConnectAsync(new("localhost:7233")
+{
+    LoggerFactory = LoggerFactory.Create(builder =>
+        builder.AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ")
+               .SetMinimumLevel(LogLevel.Information)),
+});
 
+builder.Services.AddSingleton(ctx =>client);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
@@ -38,5 +36,11 @@ app.MapGet("/", async context =>
         new(id: $"aspnet-sample-workflow-{Guid.NewGuid()}", taskQueue: "TaskQueue1"));
     await context.Response.WriteAsJsonAsync("Workflow Executed");
 });
+
+//await TrackingScheduler.ScheduleStartAsync(client);
+//await TrackingScheduler.PausescheduleAsync(client);
+//await TrackingScheduler.UnPausescheduleAsync(client);
+//await TrackingScheduler.ScheduleGoFasterAsync(client);
+//await TrackingScheduler.DeletescheduleAsync(client);
 
 app.Run();
---
+++ MyWorkFlows/Schedules/TrackingScheduler.cs
@@ -0,0 +1,83 @@
+﻿using MyWorkFlows.Workflows;
+using Temporalio.Client.Schedules;
+using Temporalio.Client;
+using Temporalio.Api.Enums.V1;
+
+namespace MyWorkFlows.Schedules;
+
+public class TrackingScheduler 
+{
+    public static async Task ScheduleStartAsync(TemporalClient client)
+    {
+        Console.WriteLine("Scheduling workflow");
+
+        var action = ScheduleActionStartWorkflow.Create<TrackingWorkflow>(wf => wf.RunAsync(), new()
+        {
+            Id = "schedule-workflow-id",
+            TaskQueue = "TaskQueue1", // Use the task queue for scheduling
+        });
+
+        var spec = new ScheduleSpec
+        {
+            Intervals = new List<ScheduleIntervalSpec>
+        {
+            new(Every: TimeSpan.FromSeconds(60)), 
+        },
+        };
+
+        var schedule = new Schedule(action, spec)
+        {
+            Policy = new()
+            {
+                CatchupWindow = TimeSpan.FromDays(1), // Define the catch-up window in case workflows are missed
+                Overlap = ScheduleOverlapPolicy.AllowAll, // Allow overlapping workflows if needed
+            },
+        };
+
+        // Here we create the schedule
+        await client.CreateScheduleAsync("Schedule 1", schedule);
+        Console.WriteLine("Workflow scheduled successfully");
+    }
+
+
+    public static async Task DeletescheduleAsync(TemporalClient client)
+    {
+        var handle = client.GetScheduleHandle("Schedule 1");
+        await handle.DeleteAsync();
+        Console.WriteLine("Schedule is now deleted.");
+    }
+
+    public static async Task PausescheduleAsync(TemporalClient client)
+    {
+        var handle = client.GetScheduleHandle("Schedule 1");
+        await handle.PauseAsync();
+        Console.WriteLine("Schedule is now deleted.");
+    }
+
+    public static async Task UnPausescheduleAsync(TemporalClient client)
+    {
+        var handle = client.GetScheduleHandle("Schedule 1");
+        await handle.UnpauseAsync();
+        Console.WriteLine("Schedule is now deleted.");
+    }
+
+    public static async Task ScheduleGoFasterAsync(TemporalClient client)
+    {
+        var handle = client.GetScheduleHandle("Schedule 1");
+
+        await handle.UpdateAsync(input =>
+        {
+            var spec = new ScheduleSpec
+            {
+                Intervals = new List<ScheduleIntervalSpec>
+            {
+                new(Every: TimeSpan.FromMinutes(5)),
+            },
+            };
+            var schedule = input.Description.Schedule with { Spec = spec };
+            return new ScheduleUpdate(schedule);
+        });
+
+        Console.WriteLine("Schedule is now triggered every 5 minutes.");
+    }
+}
